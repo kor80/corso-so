93891137884cae2dc69e241bf1ced4469e357f95
kor80 <cosimo.russo8080@gmail.com>
2022-06-02 19:10:18 +0200

    Debug lines deleted.

--- src/fishlake/FishLakeSem.java
+++ src/fishlake/FishLakeSem.java
@@ -6,7 +6,6 @@ public class FishLakeSem extends FishLakeAbstract
 {
     private Semaphore[] turnMutex=new Semaphore[2];
     private Semaphore lakeMutex=new Semaphore(1);
-    private Semaphore fishMutex=new Semaphore(1);
     private Semaphore[] fishOperation=new Semaphore[2];
     private int[] fishRequired=new int[2];
     private int[] numberOfPeopleInside=new int[2];
@@ -24,8 +23,6 @@ public class FishLakeSem extends FishLakeAbstract
         fishOperation[t].acquire(fishRequired[t]);
         turnMutex[t].acquire();
         if( numberOfPeopleInside[t]==0 ) lakeMutex.acquire();
-        if( t==EMPLOYEE_TURN ) System.out.println("Employee fills the lake.");
-        else System.out.println("Fisherman starts to fish!");
         numberOfPeopleInside[t]++;
         turnMutex[t].release();
     }//startt
@@ -34,8 +31,6 @@ public class FishLakeSem extends FishLakeAbstract
     protected void stop(int t) throws InterruptedException{
         turnMutex[t].acquire();
         numberOfPeopleInside[t]--;
-        if( t==EMPLOYEE_TURN ) System.out.println("Employee exits.");
-        else System.out.println("Fisherman ends to fish!");
         fishOperation[(t+1)%2].release(fishRequired[t]);
         if( numberOfPeopleInside[t]==0 ) lakeMutex.release();
         turnMutex[t].release();
